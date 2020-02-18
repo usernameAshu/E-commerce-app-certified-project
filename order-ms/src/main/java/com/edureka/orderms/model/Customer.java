e7a6039f901f61cda970ba41f9f606d6e9be568b
ashutosh <ashutosh@qburst.com>
2020-02-18 09:57:49 +0530

    add entity validations

--- order-ms/src/main/java/com/edureka/orderms/model/Customer.java
+++ order-ms/src/main/java/com/edureka/orderms/model/Customer.java
@@ -10,6 +10,9 @@ import javax.persistence.FetchType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
@@ -27,15 +30,22 @@ public class Customer {
 	private Long customerId;
 	
 	@Column(name="Last_Name")
+	@Pattern(regexp = "[^0-9]")
+	@NotNull(message = "Last Name shouldn't be NULL")
 	private String lastName;
 	
 	@Column(name="First_Name")
+	@Pattern(regexp = "[^0-9]")
+	@NotNull(message = "First Name shouldn't be NULL")
 	private String firstName;
 	
 	@Column(name="Username")
+	@NotNull(message = "Username shouldn't be NULL")
 	private String username;
 	
 	@Column(name="Password")
+	@NotNull(message = "Password cannot be NULL")
+	@Size(min = 3,max = 8,message = "Password has to be between 3 and 8 charecters")
 	private String password;
 	
 	@OneToMany(cascade = CascadeType.ALL, 
