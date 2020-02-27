993b9f65e986a79106af4319d1d7f8976f90a9eb
usernameAshu <mohantyashu.07@gmail.com>
2020-02-27 16:42:14 +0530

    modify oauth server

--- oauth-server/src/main/java/com/edureka/oauthserver/OauthServerApplication.java
+++ oauth-server/src/main/java/com/edureka/oauthserver/OauthServerApplication.java
@@ -2,8 +2,14 @@ package com.edureka.oauthserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
+@EnableAuthorizationServer
+@EnableResourceServer
+@EnableEurekaClient
 public class OauthServerApplication {
 
 	public static void main(String[] args) {
